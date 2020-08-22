7fb28368b47fb636c31900fb0f444b7fb97fde55
Joseph397 <gramkracker88@gmail.com>
2020-08-22 09:55:14 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+// unary operator - typeof
+let text = 'some text';
+console.log(typeof text); // operand
+// binary operator - assignment
+let number = 3;
+let number2 = 2 + 5;
+// ternary operator
+// condition ? (runs if true) : (runs if false)
+
+const value = 1 < 0;
+
+value ? console.log('value is true'):console.log('value is false');; // check weather value is true or false, if true the code before the : is run, if false the code after the : is run.
+
+
+// if(value) {
+//     console.log('value is true');
+// } else {
+//     console.log('value is false');
+// }
